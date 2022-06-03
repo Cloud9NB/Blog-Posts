88df84cd91a6f701418a664454a1781621057f4f
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-06-02 21:14:25 -0700

    filered api depending on the params of sortby and direction

--- express-back-end/src/routes/api.js
+++ express-back-end/src/routes/api.js
@@ -9,23 +9,45 @@ module.exports = () => {
     res.status(200).json(apiPing);
   });
   
-  router.get('/posts/:tags/:sortBy/:direction', (req, res) => {
+  router.get('/posts/:tags/:sortBy?/:direction?', (req, res) => {
     const { tags, sortBy, direction } = req.params;
     const tagsArray = tags.split(',');
 
-    const route = tagsArray.map(tag => {
+    const route = tagsArray.map((tag, index) => {
       return axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${tag}&sortBy=${sortBy}&direction=${direction}`);
     });
-    
     Promise.all([
       ...route
     ])
     .then(all => {
-      res.status(200).json(all[0].data);
-    })
+      const tagOne = all[0].data.posts;
+      const tagTwo = all[1].data.posts;
+      const combinedData = [...tagOne, ...tagTwo];
+      const filteredArray = combinedData.filter((tag, index, self) => {
+        return index === self.findIndex(post => {
+          return post.id === tag.id;
+        });
+      });
+
+      
+      if (sortBy) {
+        if (direction === 'asc') {
+          filteredArray.sort((a, b) => b[sortBy] < a[sortBy] ? 1 : -1)
+        } else {
+          filteredArray.sort((a, b) => b[sortBy] > a[sortBy] ? 1 : -1)
+        }
+      }
+      
+      let newData = {
+        posts: filteredArray,
+      };
+
+
+      res.status(200).json(newData);
+      })
     .catch(error => {
-      res.status(400).json(error.response.data);
-      // console.log('The error is: ', error);
+      // res.status(400).json(error.response.data);
+      console.log('The error is: ', error);
     });
   });
 
